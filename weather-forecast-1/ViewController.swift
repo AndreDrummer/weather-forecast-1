8fee939f173ff43c625d0f3bd1c97c30c4a3131b
AndréDrummer <academicworks.adf@gmail.com>
2024-08-19 19:59:41 -0300

    first commit

---
+++ weather-forecast-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-forecast-1
+//
+//  Created by Andre Silva on 19/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
